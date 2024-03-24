aabb797d9c81471afc52c75b43d1a1d0ad1abe8d
Jezae Thomas (thoma339) <thoma339@coventry.ac.uk>
2024-03-24 07:35:27 +0000

    logout done

--- app/src/main/java/com/example/greenrover/MainActivity.java
+++ app/src/main/java/com/example/greenrover/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.greenrover;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,12 +21,16 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
     FloatingActionButton addBtn;
     private FirebaseAuth mAuth;
     private FirebaseDatabase database;
+    msgPopup d;
+    Dialog logoutDialog;
 
 
     @Override
@@ -37,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         addBtn = findViewById(R.id.addBtn);
+        d = new msgPopup();
 
         replaceFragment(new HomeFragment());
         binding.bottomNavigationView.setBackground(null);
@@ -49,7 +58,7 @@ public class MainActivity extends AppCompatActivity {
             } else if (itemId == R.id.history) {
                 replaceFragment(new HistoryFragment());
             } else if (itemId == R.id.logout) {
-                replaceFragment(new logoutFragment());
+                showlogoutScreen();
             }
             return true;
         });
@@ -71,4 +80,37 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra("name","info");
         startActivity(intent);
     }
+
+    public void showlogoutScreen(){
+        logoutDialog = new Dialog(MainActivity.this);
+
+        Objects.requireNonNull(logoutDialog.getWindow()).setBackgroundDrawableResource(android.R.color.transparent);
+        logoutDialog.setContentView(R.layout.logout_box);
+
+        WindowManager.LayoutParams par = new WindowManager.LayoutParams();
+        par.copyFrom(logoutDialog.getWindow().getAttributes());
+        par.width = WindowManager.LayoutParams.MATCH_PARENT;
+        par.height = WindowManager.LayoutParams.WRAP_CONTENT;;
+        logoutDialog.getWindow().setAttributes(par);
+        logoutDialog.setCancelable(false);
+        logoutDialog.setCanceledOnTouchOutside(false);
+        Button btn = (Button) logoutDialog.findViewById(R.id.msg_box_btn);
+        Button btn2 = (Button) logoutDialog.findViewById(R.id.logout_box_btn);
+
+        btn.setOnClickListener(v -> {
+            logoutDialog.dismiss();
+        });
+
+        btn2.setOnClickListener(v -> {
+            logoutDialog.dismiss();
+            mAuth.signOut();
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+        });
+
+        logoutDialog.show();
+
+    }
+
 }
